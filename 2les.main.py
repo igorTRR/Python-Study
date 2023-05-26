9fa8695502ad0be6832143fab6921b9c324c3a21
Igor <tararuev65@gmail.com>
2023-05-26 22:48:31 +0600

    Les2

---
+++ 2les.main.py
@@ -0,0 +1,12 @@
+list_1=[2, 4, 12, 23, 6]
+print(list_1)
+# print(list_1.pop())
+# print(list_1)
+# print(list_1.pop(1))
+# print(list_1)
+# a = list_1.pop(2)
+# print(list_1)
+# print(a)
+# print(list_1)
+t = ()
+print(type(t))
