2e42019d7df49aa057839cbe9df3a9b3840b56c5
Igor <tararuev65@gmail.com>
2023-05-31 09:38:31 +0600

    Z27_Кол.слов в text

--- 4Sem_Z27.main.py
+++ 4Sem_Z27.main.py
@@ -4,11 +4,15 @@
 # пробелов или символами конца строки.Определите, сколько различных
 # слов содержится в этом тексте.
 
-text = "She sells, sea shells on the sea shore. The shells\
+text = '''She sells, sea shells on the sea shore. The shells\
  that she sells are sea shells! I'm sure So if she sells sea \
 shells on the sea shore? I'm sure that\n the shells are sea \
-shore shells"
-result = [i.strip('.,?!\n').lower() for i in text.split()]
-result = set(result)
-print(result)
-print(len(result))
+shore shells'''.upper().replace ('.','' ).split()
+# result = [i.strip('.,?!\n').lower() for i in text.split()]
+# result = set(result)
+# print(result)
+# print(len(result))
+print(text)
+# print(len(text))
+print (len(set(text)))
+
