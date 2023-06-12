9d6e446772b9e1a259fcb5d09c1ba4d6ffd75170
Igor <tararuev65@gmail.com>
2023-06-12 22:29:32 +0600

    Генератор+map

---
+++ Les_генер.py
@@ -0,0 +1,14 @@
+list_1 = [i for i in range(1,20) ]
+print(list_1)
+
+list_1 = list(map(lambda i: i + 10, list_1))
+print(list_1)
+
+data = '15 156 46 7 9 65'
+print(data)
+
+# data =data.split()
+# print(data)
+
+data = list(map(int,data.split()))# map -встроенная f можно пройти 1раз
+print(data)
