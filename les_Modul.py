a7c59930d8c105c606167df81026038d679df579
Igor <tararuev65@gmail.com>
2023-06-12 10:59:09 +0600

    odul max

--- les_Modul.py
+++ les_Modul.py
@@ -1,2 +1,4 @@
-import modul1
-print(modul1.max1(30, 8))
+#import modul1                   # from modul1 import * 
+#print(modul1.max1(15, 8))       # print(max1(15, 8))   
+import modul1 as res
+print(res.max1(5, 8))
